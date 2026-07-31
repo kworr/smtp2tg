4db5fc3613b134dfae93299af611f3dcd7567dab
arcade <arcade@b1t.name>
2026-07-31 10:42:58 +0000

    reorganize to lib + bin to separate tests, add logging, fix some small stuff
    FossilOrigin-Name: 3c858ed7c4bad10014f3480fccef1320679f97a891d5ef1ccabbc9d7cad12114

--- src/utils.rs
+++ src/utils.rs
@@ -11,11 +11,13 @@ use stacked_errors::{
 };
 
 lazy_static! {
-	pub static ref RE_DOMAIN: Regex = Regex::new(r"^[a-z0-9]([-a-z0-9]*[a-z0-9])?(\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*$").unwrap();
-	pub static ref RE_CLOSING: Regex = Regex::new(r"</[ \t]*(pre|code)[ \t]*>").unwrap();
+	pub static ref RE_DOMAIN: Regex = Regex::new(r"^[a-z0-9]([-a-z0-9]*[a-z0-9])?(\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*$").expect("Invalid domain regex");
+	pub static ref RE_CLOSING: Regex = Regex::new(r"</[ \t]*(pre|code)[ \t]*>").expect("Invalid cloding tag regex");
 }
 
-/// `Attachment` object to store number attachment data and corresponding file name
+/// Stores binary attachment data and metadata for Telegram messages.
+/// The data is wrapped in a `Cursor<Vec<u8>>` for efficient streaming,
+/// while `name` holds the filename or display name of the attachment.
 #[derive(Debug)]
 pub struct Attachment {
 	pub data: Cursor<Vec<u8>>,
@@ -24,7 +26,7 @@ pub struct Attachment {
 
 /// Pass any text here to be validated as not breaking from Telegram preformatted blocks
 /// escape all HTML chars afterwards
-pub fn validate (text: &str) -> Result<Cow<'_, str>> {
+pub fn validate <'a>(text: &'a str) -> Result<Cow<'a, str>> {
 	if RE_CLOSING.is_match(text) {
 		bail!("Telegram closing tag found.");
 	} else {
